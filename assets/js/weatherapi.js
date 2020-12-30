34e93300b6cd7a82f8ea2f7642a27b4aaed5dfc7
alanantony24 <s10207327@connect.np.edu.sg>
2020-12-30 19:28:59 +0800

    implementing weather data into html

--- assets/js/weatherapi.js
+++ assets/js/weatherapi.js
@@ -1,6 +1,6 @@
 const appID = 'e4eb93ab8f89ca443bd386dd864ee705'
 const input = document.querySelector(".locationinput input")
-document.getElementById("locbutton").addEventListener("click", e =>{
+document.getElementById("resultbtn").addEventListener("click", e =>{
     e.preventDefault()
     const inputval = input.value;
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputval}&appid=${appID}&units=metric`
@@ -8,13 +8,12 @@ document.getElementById("locbutton").addEventListener("click", e =>{
         .then(response => response.json())
         .then(data => {
             const{main, name, sys, weather} = data;
-            document.getElementById("locbutton").onclick = function(){myfunction()};
+            document.getElementById("resultbtn").onclick = function(){myfunction()};
             function myfunction(){
-            document.getElementById("temps").innerHTML = main.temp;
-            document.getElementById("weatherdesc").innerHTML = weather[0].description;
+                document.getElementById("temp").innerHTML = 'The temperature today is ' + main.temp + '&deg;C';
+                document.getElementById("weatherdesc").innerHTML = 'Weather description : ' + weather[0].description;s
+                document.getElementById("countryname").innerHTML = name + ',' +  sys.country;
          }
      })
 
 })
-
-
