b5696d57b168fbe27347cd0173f2c55e7f82c264
alanantony24 <s10207327@connect.np.edu.sg>
2021-01-07 12:17:55 +0800

    fixed a bug with the statements

--- assets/js/weatherapi.js
+++ assets/js/weatherapi.js
@@ -1,6 +1,7 @@
 
 const appID = 'e4eb93ab8f89ca443bd386dd864ee705';
 const input = document.querySelector(".locationinput input")
+const name = document.getElementById("exampleInputName").value;
 var clothesinfo = [
     {
       "_id": "5ff2902a74fa860c00008523",
@@ -65,6 +66,7 @@ document.getElementById("resultbtn").addEventListener("click", e=> {
         .then(response => response.json())
         .then(data => {
             const{main, name, wind, weather} = data;
+            document.getElementById("clotheshead").innerHTML = "Hey " + name + ", here's our recommendation for today's clothing! :)"
             document.getElementById("temp").innerHTML = main.temp + '&deg;C';
             document.getElementById("country").innerHTML = name;
             document.getElementById("desc").innerHTML = weather[0].description;
@@ -106,7 +108,7 @@ document.getElementById("resultbtn").addEventListener("click", e=> {
                 document.getElementById("weatherapp").style.background = ' #8aa7ec';
                 return 'icons/chancerain.png'
             }
-            else if(weather[0].description == 'drizzle' || weather[1].description == 'light intensity drizzle' || weather[0].description == 'light intensity shower rain' || weather[0].description == 'shower rain'){
+            else if(weather[0].description == 'drizzle' || weather[0].description == 'light intensity drizzle' || weather[0].description == 'light intensity shower rain' || weather[0].description == 'shower rain'){
                 document.getElementById("weatherapp").style.background = 'green';
                 return 'icons/chancerain.png'
             }
@@ -115,48 +117,47 @@ document.getElementById("resultbtn").addEventListener("click", e=> {
                 return 'icons/unknown.png'
             }
             }
-            function GetClothes(){
-              if(main.temp >= 25){
-                document.getElementById("topwear").innerHTML = clothesinfo[0].Top;
-                document.getElementById("bottomwear").innerHTML = clothesinfo[0].Bottom;
-                document.getElementById("footwear").innerHTML = clothesinfo[0].Footwear;
-                document.getElementById("accessories").innerHTML = clothesinfo[0].Accessories;
+            function getClothes(){
+              if(weather[0].description == 'light rain' || weather[0].description == 'drizzrle' || weather[0].description == 'light intensity drizzle' || weather[0].description == 'light intensity shower rain' || weather[0].description == 'shower rain'){
+                document.getElementById("topwear").innerHTML = clothesinfo[1].Top;
+                document.getElementById("bottomwear").innerHTML = clothesinfo[1].Bottom;
+                document.getElementById("footwear").innerHTML = clothesinfo[1].Footwear;
+                document.getElementById("accessories").innerHTML = clothesinfo[1].Accessories;
               }
-              else if(main.temp <= 15 && main.temp >= 5){
-                document.getElementById("topwear").innerHTML = clothesinfo[4].Top;
-                document.getElementById("bottomwear").innerHTML = clothesinfo[4].Bottom;
-                document.getElementById("footwear").innerHTML = clothesinfo[4].Footwear;
-                document.getElementById("accessories").innerHTML = clothesinfo[4].Accessories;
-              }
-              else if(main.temp < 5){
-                document.getElementById("topwear").innerHTML = clothesinfo[5].Top;
-                document.getElementById("bottomwear").innerHTML = clothesinfo[5].Bottom;
-                document.getElementById("footwear").innerHTML = clothesinfo[5].Footwear;
-                document.getElementById("accessories").innerHTML = clothesinfo[5].Accessories;
-              }
-            }
-            function ifRain(){
-              if(weather[0].description == 'moderate rain' || wewather[0].description == 'thunderstorm' || weather[0].description == 'heavy intensity rain'){
+              else if(weather[0].description == 'thunderstorm' || weather[0].description == 'heavy intensity rain' || weather[0].description == 'moderate rain'){
                 document.getElementById("topwear").innerHTML = clothesinfo[2].Top;
                 document.getElementById("bottomwear").innerHTML = clothesinfo[2].Bottom;
                 document.getElementById("footwear").innerHTML = clothesinfo[2].Footwear;
                 document.getElementById("accessories").innerHTML = clothesinfo[2].Accessories;
               }
-              else if(weather[0].description == 'drizzle' || weather[1].description == 'light intensity drizzle' || weather[0].description == 'light intensity shower rain' || weather[0].description == 'shower rain'){
-                document.getElementById("topwear").innerHTML = clothesinfo[1].Top;
-                document.getElementById("bottomwear").innerHTML = clothesinfo[1].Bottom;
-                document.getElementById("footwear").innerHTML = clothesinfo[1].Footwear;
-                document.getElementById("accessories").innerHTML = clothesinfo[1].Accessories;
-              }
-              else if(weather[0].description == 'light rain'){
-                document.getElementById("topwear").innerHTML = clothesinfo[1].Top;
-                document.getElementById("bottomwear").innerHTML = clothesinfo[1].Bottom;
-                document.getElementById("footwear").innerHTML = clothesinfo[1].Footwear;
-                document.getElementById("accessories").innerHTML = clothesinfo[1].Accessories;
+              else{
+                if(main.temp >= 25){
+                  document.getElementById("topwear").innerHTML = clothesinfo[0].Top;
+                  document.getElementById("bottomwear").innerHTML = clothesinfo[0].Bottom;
+                  document.getElementById("footwear").innerHTML = clothesinfo[0].Footwear;
+                  document.getElementById("accessories").innerHTML = clothesinfo[0].Accessories;
+                }
+                else if(main.temp < 25 && main.temp >=20){
+                  document.getElementById("topwear").innerHTML = clothesinfo[3].Top;
+                  document.getElementById("bottomwear").innerHTML = clothesinfo[3].Bottom;
+                  document.getElementById("footwear").innerHTML = clothesinfo[3].Footwear;
+                  document.getElementById("accessories").innerHTML = clothesinfo[3].Accessories;
+                }
+                else if(main.temp < 20 && main.temp >=15){
+                  document.getElementById("topwear").innerHTML = clothesinfo[4].Top;
+                  document.getElementById("bottomwear").innerHTML = clothesinfo[4].Bottom;
+                  document.getElementById("footwear").innerHTML = clothesinfo[4].Footwear;
+                  document.getElementById("accessories").innerHTML = clothesinfo[4].Accessories;
+                }
+                else if(main.temp < 15){
+                  document.getElementById("topwear").innerHTML = clothesinfo[5].Top;
+                  document.getElementById("bottomwear").innerHTML = clothesinfo[5].Bottom;
+                  document.getElementById("footwear").innerHTML = clothesinfo[5].Footwear;
+                  document.getElementById("accessories").innerHTML = clothesinfo[5].Accessories;
+                }
               }
             }
-            GetClothes();
-            ifRain();
+            getClothes();
         })
       })
 
